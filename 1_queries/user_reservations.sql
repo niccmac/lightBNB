1834eee54991150fc701f9ce54bc2af9ce714f7e
Nicole MacLean <nicole.mac0404@gmail.com>
2022-04-06 18:25:02 -0700

    Add query user reservations

---
+++ 1_queries/user_reservations.sql
@@ -0,0 +1,9 @@
+/* Returns table of all reservations user has */
+SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, avg(property_reviews.rating) AS average_ratings
+FROM reservations
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 1
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
